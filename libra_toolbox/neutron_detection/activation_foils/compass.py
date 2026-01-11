8f60aa5ca21c7292e70f155550b0781f1266c578
Collin Dunn <cdunn314@mit.edu>
2026-01-11 00:54:22 -0500

    Debugged for Na22 and Mn54 test cases

--- libra_toolbox/neutron_detection/activation_foils/compass.py
+++ libra_toolbox/neutron_detection/activation_foils/compass.py
@@ -522,8 +522,8 @@ class CheckSourceMeasurement(Measurement):
             distance = 30
             if self.check_source.nuclide == na22:
                 start_index = 100
-                height = 0.1 * np.max(hist[start_index:])
-                prominence = 0.1 * np.max(hist[start_index:])
+                height = 0.4 * np.max(hist[start_index:])
+                prominence = 0.4 * np.max(hist[start_index:])
                 width = [10, 150]
                 distance = 30
             elif self.check_source.nuclide == co60:
@@ -545,8 +545,8 @@ class CheckSourceMeasurement(Measurement):
             distance = 100
             if self.check_source.nuclide == na22:
                 start_index = 100
-                height = 0.2 * np.max(hist[start_index:])
-                prominence = 0.2 * np.max(hist[start_index:])
+                height = 0.4 * np.max(hist[start_index:])
+                prominence = 0.4 * np.max(hist[start_index:])
                 distance = 100
             elif self.check_source.nuclide == co60:
                 height = 0.5 * np.max(hist[start_index:])
@@ -558,8 +558,8 @@ class CheckSourceMeasurement(Measurement):
                 distance = 10
             elif self.check_source.nuclide == mn54:
                 start_index = 400
-                height = 0.9 * np.max(hist[start_index:])
-                prominence = 0.9 * np.max(hist[start_index:])
+                height = 0.7 * np.max(hist[start_index:])
+                prominence = 0.7 * np.max(hist[start_index:])
                 distance = 100
         else:
             raise ValueError(
@@ -584,6 +584,10 @@ class CheckSourceMeasurement(Measurement):
             distance=distance,
         )
         peaks = np.array(peaks) + start_index
+
+        # special case for Mn-54, only keep the first high count energy peak
+        if self.check_source.nuclide == mn54 and len(peaks) > 1:
+            peaks = np.array([peaks[0]])
 
         return peaks
 
